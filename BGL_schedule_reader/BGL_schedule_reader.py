516d202b0154a0a7e7d10bc597f259df16ece600
Demiko <dmserbin@outlook.com>
2019-09-18 14:43:26 +0300

    added GameEvent object class

--- BGL_schedule_reader/BGL_schedule_reader.py
+++ BGL_schedule_reader/BGL_schedule_reader.py
@@ -2,41 +2,23 @@ from bs4 import BeautifulSoup
 import requests as req
 import time
 import re
+from game_event import GameEvent
 
-calendar = "https://club.bgl.com.ua/calendar/"
+calendarBaseURL = "https://club.bgl.com.ua/calendar/"
 
 def main():
     day = list(time.localtime())
-    schedule_page = req.get(calendar + time.strftime('%Y-%m-%d', tuple(day)))
-    events = BeautifulSoup(schedule_page.text, 'lxml').find(class_='calendar-events')
-
+    schedulePage = req.get(calendarBaseURL + time.strftime('%Y-%m-%d', tuple(day)))
+    events = BeautifulSoup(schedulePage.text, 'lxml').find(class_='calendar-events')
     for event in events.find_all(class_='event-item'):
-
-        for glyph in event.find_all(class_='glyphicon'):
-            glyph.extract()
-
-        ev_id = re.search(r'\d+', event.find(class_='btn')['href']).group()
-        print('ID: %s' % ev_id)
-
-        ev_title = event.find(class_="event-item-title")
-        print('Event: %s' % ev_title.a.text.strip())
-
-        ev_type = event.find(class_='event-type')
-        print('Type: %s' % ev_type.text.strip())
-
-        ev_time = event.find(class_='event-item-time')
-        print('Time: %s' % ev_time.text.strip())
-
-        ev_users = event.find(class_='event-item-users')
-        ev_seats = re.search(r'\d+', ev_users.text);
-        ev_seats = ev_seats.group() if ev_seats is not None else '0'
-        print('Free seats: %s' % ev_seats)
-
-        ev_link = 'https://club.bgl.com.ua' + event.find(class_='btn')['href']
-        print('Link: %s' % ev_link)
-
+        ge = GameEvent(event)
+        print('ID: %s' % ge.id)
+        print('Event: %s' % ge.title)
+        print('Type: %s' % ge.type)
+        print('Time: %s' % ge.time)
+        print('Free seats: %s' % ge.seats)
         print()
-
+        pass
     pass
 
 if __name__ == "__main__":
---
+++ BGL_schedule_reader/game_event.py
@@ -0,0 +1,28 @@
+from bs4.element import Tag
+import re
+
+class GameEvent:
+    """an object representing data about BGL game event"""
+    def __init__(self, *args, **kwargs):
+        if args and type(args[0]) is Tag :
+            event=args[0]
+            # glyphicons are noize
+            for glyph in event.find_all(class_='glyphicon'): glyph.decompose()
+            try: self.id = re.search('\d+', event.find(class_='btn')['href']).group()
+            except: self.id = 'id not found'
+            try: self.title = event.find(class_="event-item-title").a.text.strip()
+            except: self.title = 'title not found'
+            try:
+                # Should change this later to read the class tag.
+                # Reason: because MTG has one type but several different type titles.
+                self.type = event.find(class_='event-type').text.strip()
+            except: self.type = 'type not found'
+            try: self.time = event.find(class_='event-item-time').text.strip()
+            except: self.time = 'time not found'
+            try:
+                tmp = re.search(r'\d+', event.find(class_='event-item-users').text)
+                self.seats = tmp.group() if tmp else '0'
+                del tmp
+            except: self.seats = 'seats not found'
+        pass
+pass
