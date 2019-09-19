5c5168dd8f06de7e5f3762f465663898718c11d9
Demiko <dmserbin@outlook.com>
2019-09-19 21:43:59 +0300

    Added GameDay

---
+++ BGL_schedule_reader/GameDay.py
@@ -0,0 +1,19 @@
+from GameEvent import GameEvent
+from datetime import date
+from bs4 import BeautifulSoup
+import requests as req
+
+calendarBaseURL = "https://club.bgl.com.ua/calendar/"
+
+class GameDay:
+    """description of class"""
+    def __init__(self, date: date):
+        self.events = []
+        schedulePage = req.get(calendarBaseURL + date.isoformat())
+        events = BeautifulSoup(schedulePage.text, 'lxml').find(class_='calendar-events')
+        for event in events.find_all(class_='event-item'):
+            ge = GameEvent(event)
+            self.events.append(ge)
+            pass
+        pass
+    pass
